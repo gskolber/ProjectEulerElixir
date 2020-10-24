caf144a343b05529004f588cb54c2dc1d337a57e
gabriel kolber <gabrielkolber@icloud.com>
2020-10-24 15:13:18 -0300

    feat: added project euler question 3

--- project_euler03/lib/project_euler03.ex
+++ project_euler03/lib/project_euler03.ex
@@ -4,15 +4,32 @@ defmodule ProjectEuler03 do
   """
 
   @doc """
-  Hello world.
+  Largest prime factor
+
+  The prime factors of 13195 are 5, 7, 13 and 29.
+  What is the largest prime factor of the number 600851475143 ?
 
   ## Examples
 
-      iex> ProjectEuler03.hello()
-      :world
+      iex> ProjectEuler03.largest_prime_factor(10)
+      5
 
   """
-  def hello do
-    :world
+  def largest_prime_factor(number) do
+    number
+    |>is_prime(2,[])
+    |>Enum.max()
   end
+
+  defp is_prime(number, divider, list) when divider > number do
+    list
+  end
+
+  defp is_prime(number, divider, list) when rem(number, divider) ==0 do
+    is_prime(div(number,divider), divider, [divider | list])
+  end
+  defp is_prime(number, divider, list) when rem(number, divider) !=0 do
+    is_prime(number,divider+1,list)
+  end
+
 end
--- project_euler03/test/project_euler03_test.exs
+++ project_euler03/test/project_euler03_test.exs
@@ -4,5 +4,6 @@ defmodule ProjectEuler03Test do
 
   test "largest prime factor" do
     assert ProjectEuler03.largest_prime_factor(13195) == 29
+    assert ProjectEuler03.largest_prime_factor(600851475143) == 6857
   end
 end
