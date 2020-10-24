bb0f2f52cedf63b5ee516eda07e4d3e3b20ce687
gabriel kolber <gabrielkolber@icloud.com>
2020-10-24 15:37:48 -0300

    feat:exercise 04, tests and documentation added

--- project_euler04/lib/project_euler04.ex
+++ project_euler04/lib/project_euler04.ex
@@ -4,15 +4,38 @@ defmodule ProjectEuler04 do
   """
 
   @doc """
-  Hello world.
+  Largest palindrome product
+
+  A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+  Find the largest palindrome made from the product of two 3-digit numbers.
 
   ## Examples
 
-      iex> ProjectEuler04.hello()
-      :world
+      iex> ProjectEuler04.palindrome(99)
+      9009
 
   """
-  def hello do
-    :world
+  def palindrome(range) do
+    range
+    |> product(range)
   end
+
+  defp product(x, y) do
+    result = x * y
+    case to_string(result) == is_palindrome(result) do
+      true -> result
+      false -> product(x, y-1)
+    end
+
+  end
+
+  defp is_palindrome(number) do
+
+    String.reverse(to_string(number))
+
+  end
+
+
+
 end
--- project_euler04/test/project_euler04_test.exs
+++ project_euler04/test/project_euler04_test.exs
@@ -2,7 +2,8 @@ defmodule ProjectEuler04Test do
   use ExUnit.Case
   doctest ProjectEuler04
 
-  test "greets the world" do
+  test "largest palindrome" do
     assert ProjectEuler04.palindrome(99) == 9009
   end
+
 end
