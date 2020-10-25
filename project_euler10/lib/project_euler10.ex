611ab7231e71c782ca8c67523ac548b8da4c1a63
gabriel kolber <gabrielkolber@icloud.com>
2020-10-25 15:39:17 -0300

    feat:added function for exercise10

--- project_euler10/lib/project_euler10.ex
+++ project_euler10/lib/project_euler10.ex
@@ -16,7 +16,37 @@ defmodule ProjectEuler10 do
       17
 
   """
-  def hello do
-    :world
+  def sum_of_primes(range) do
+    range
+    |> number_is_prime?(0, 2)
+
   end
+
+  defp number_is_prime?(range, list, acc) when acc==range do
+    list
+  end
+
+
+  defp number_is_prime?(range, list, acc) when acc<=range do
+
+    case is_prime(acc, 2) do
+      true ->
+        number_is_prime?(range, list+acc, acc+1)
+      false ->
+        number_is_prime?(range, list, acc+1)
+
+      end
+  end
+
+  defp is_prime(number, acc) when number > acc do
+    case rem(number, acc) == 0 do
+      true -> false
+      false -> is_prime(number, acc+1)
+    end
+  end
+
+  defp is_prime(number, acc) when number == acc do
+    true
+  end
+
 end
--- project_euler10/test/project_euler10_test.exs
+++ project_euler10/test/project_euler10_test.exs
@@ -1,7 +1,7 @@
 defmodule ProjectEuler10Test do
   use ExUnit.Case
   doctest ProjectEuler10
-
+  @tag timeout: :infinity
   test "sum of primes bellow:" do
     assert ProjectEuler10.sum_of_primes(10) == 17
   end
